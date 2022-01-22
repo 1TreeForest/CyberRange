e8b5bba3f7cc585355090b9be4506f4e50bb3b00
1TreeForest <1204572840@qq.com>
2022-01-23 00:32:29 +0800

    debug

--- siteCrawler/siteCrawler/spiders/keywordSpider.py
+++ siteCrawler/siteCrawler/spiders/keywordSpider.py
@@ -84,6 +84,6 @@ class KeywordSpider(Spider):
             elif self.search_engine == 'google':  # 谷歌的结果无需进行额外处理
                 item['name'] = names[i]
                 item['url'] = urls[i]
-                item['domain'] = re.search(r'://(.+?)/', item['url']).group(1)  # 正则匹配提取链接的主要部分，用来判断是否已存在该网站的爬取结果
-                
+
+            item['domain'] = re.search(r'://(.+?)/', item['url']).group(1)  # 正则匹配提取链接的主要部分，用来判断是否已存在该网站的爬取结果
             yield item
