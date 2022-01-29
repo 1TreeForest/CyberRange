9f6de7d0bc96ad42744e2a1409032b694f953a07
1TreeForest <1204572840@qq.com>
2022-01-29 15:02:02 +0800

    真的看不出来为何大量的数据无法更新

--- siteCrawler/siteCrawler/pipelines.py
+++ siteCrawler/siteCrawler/pipelines.py
@@ -26,40 +26,25 @@ class SiteCrawlerPipeline(object):
             user='test',
             password='991125',
             db='spider',
-            charset='UTF8'
+            charset='UTF8',
+            autocommit=True
         )
         self.cursor = self.conn.cursor()
 
     def process_item(self, item, spider):
-        try:
-            self.conn.ping(reconnect=True)
-        except Exception as e:
-            self.db_connect()
-            logging.warning("数据库已重新连接")
-        # print(item['url']+'\n')
-        # self.file.write(item['url'] + '\n')
+        self.conn.ping(reconnect=True)
         if isinstance(item, ResultItem):
             # 拼接insert SQL语句，把每项数据的5个属性填充到SQL中作为参数
-            try:  # 此项在插入时若数据库中已存在该主键对，则进行更新操作
-                sql = 'INSERT INTO `results`(domain, name, url, keyword, crawledDate, aliveDate)VALUES("%s","%s","%s","%s","%s", "%s")' % (
-                    item['domain'], item['name'], item['url'], item['keyword'], item['crawledDate'], item['aliveDate'])
-                # 执行
-                self.cursor.execute(sql)
-                # 提交事务
-                self.conn.commit()
-                logging.info('已进行 {} 次数据采集\t\t获取到新对象: {}'.format(self.count, item['domain']))
-            except Exception as e:
-                sql = 'UPDATE `results` SET name="%s", url="%s", aliveDate="%s" WHERE domain="%s" AND keyword="%s" ' \
-                      % (item['name'], item['url'], item['aliveDate'], item['domain'], item['keyword'])
-                # 执行
-                self.cursor.execute(sql)
-                # 提交事务
-                self.conn.commit()
-                logging.info('已进行 {} 次数据采集\t{} 的信息已更新'.format(self.count, item['domain']))
-            self.count += 1
-            # sql = 'select name from `results` where domain="%s" and keyword="%s"' % (item['domain'], item['keyword'])
+            # 在插入时若数据库中已存在该主键对，则进行更新操作
+            sql = 'INSERT INTO `spider`.`results`(`domain`, `name`, `url`, `keyword`, `crawledDate`, `aliveDate`) VALUES("%s","%s","%s","%s","%s","%s") ' \
+                  'ON DUPLICATE KEY UPDATE `aliveDate`="%s";' % (
+                      item['domain'], item['name'], item['url'], item['keyword'], item['crawledDate'],
+                      item['aliveDate'], item['aliveDate'])
             # 执行
-            # self.cursor.execute(sql)
+            affected_rows = self.cursor.execute(sql)
+            print('{}\n{}'.format(sql, affected_rows))
             # 提交事务
             # self.conn.commit()
+            logging.info('已进行 {} 次数据采集'.format(self.count))
+            self.count += 1
             print(item)
--- siteCrawler/siteCrawler/spiders/keywordSpider.py
+++ siteCrawler/siteCrawler/spiders/keywordSpider.py
@@ -31,14 +31,15 @@ class KeywordSpider(Spider):
                 user='test',
                 password='991125',
                 db='spider',
-                charset='UTF8'
+                charset='UTF8',
+                autocommit=True
             )
             self.cursor = self.conn.cursor()
-            sql = 'SELECT keyword FROM querys'
+            sql = 'SELECT keyword FROM `spider`.`querys`;'
             # 执行
             self.cursor.execute(sql)
             # 提交事务
-            self.conn.commit()
+            # self.conn.commit()
             self.keyword_list = self.cursor.fetchall()[:]  # 定义爬取范围
         else:  # 用户制定了一个keyword进行搜索
             self.keyword_list = [(keyword,)]
