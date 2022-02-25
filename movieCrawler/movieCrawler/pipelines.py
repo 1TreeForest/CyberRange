b9a2cbfe5f0e94c2df5048c6433c07dde4184675
y0urOy <55057990+y0urOy@users.noreply.github.com>
2022-02-25 14:25:22 +0800

    Add files via upload

--- movieCrawler/movieCrawler/pipelines.py
+++ movieCrawler/movieCrawler/pipelines.py
@@ -1,5 +1,10 @@
+import json
+import pymysql
+from movieCrawler.items import MoviecrawlerItem
 import codecs
-import json # 导入json模块
+import logging
+
+# 导入json模块
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
@@ -10,18 +15,51 @@ import json # 导入json模块
 from itemadapter import ItemAdapter
 
 
-class MoviecrawlerPipeline:
-    # 初始化函数，创建json文件，指定编码格式
+class MoviecrawlerPipeline(object):
+    conn = None
+    cursor = None
+    count = None
+
     def __init__(self):
-        self.filename = codecs.open('dytt.json', 'w', encoding='utf-8')
+        self.db_connect()
+        self.count = 0
+
+    def db_connect(self):
+        self.conn = pymysql.Connect(  # 配置数据库
+            host='1.15.220.155',
+            port=3306,
+            user='test',
+            password='991125',
+            db='spider',
+            charset='UTF8'
+        )
+        self.cursor = self.conn.cursor()
 
     # 将item写入，在结尾增加换行符
     def process_item(self, item, spider):
-        content = json.dumps(dict(item), ensure_ascii=False) + '\n'
-        self.filename.write(content)
-        # 解析一个存一个，返回item继续跟进
-        return item
+        try:
+            self.conn.ping(reconnect=True)
+        except Exception as e:
+            self.db_connect()
+            logging.warning("数据库已重新连接")
 
-    # 爬问后就把文件关了，释放资源
-    def spider_close(self, spider):
-        self.filename.close()
+            # 拼接insert SQL语句，把每项数据的5个属性填充到SQL中作为参数
+            sql = 'INSERT INTO `movies`(name, link, site)VALUES("%s","%s","%s")' % (
+                item['name'], item['link'], item['site'])
+            # 执行
+            self.cursor.execute(sql)
+            # 提交事务
+            self.conn.commit()
+            logging.info('已进行 {} 次数据采集\t\t获取到新对象: {}，{}'.format(self.count, item['name'], item['link']))
+            self.count += 1
+            print(item)
+
+        # content = json.dumps(dict(item), ensure_ascii=False) + '\n'
+        # self.filename.write(content)
+        # # 解析一个存一个，返回item继续跟进
+        # return item
+
+    # # 爬问后就把文件关了，释放资源
+    # def spider_close(self, spider):
+    #     self.filename.close()
+
