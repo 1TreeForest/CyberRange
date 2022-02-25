ebd0e2679e4833ef1cbbf5d1360f7dee6c8b39a9
y0urOy <55057990+y0urOy@users.noreply.github.com>
2022-02-25 14:26:07 +0800

    Add files via upload

--- movieCrawler/movieCrawler/spiders/dytt.py
+++ movieCrawler/movieCrawler/spiders/dytt.py
@@ -1,5 +1,6 @@
 import scrapy
 from movieCrawler.items import MoviecrawlerItem
+import re
 
 class DyttSpider(scrapy.Spider):
     #爬虫名
@@ -21,11 +22,13 @@ class DyttSpider(scrapy.Spider):
         for i in range(0, len(name)):
             # 实例化一个item类，然后将结果全部存入items
             item = MoviecrawlerItem()
-            item['name'] = name[i]
+            #提取出的电影名中包含其他信息，如”2012年喜剧《男人如衣服》720p.BD国粤双语中字“，需进一步使用正则匹配出书名号中的电影名
+            item['name'] = re.findall('《(.*?)》', name[i])[0]
             # 拼接URL，获取所有电影详情的URL，拼成完整的URL来访问地址
             item['link'] = 'http://www.dytt8.net' + link[i]
             item['site'] = 'http://www.dytt8.net'
-            print(item)
+            # print(item)
+            yield item
 
             items.append(item)
 
@@ -34,4 +37,3 @@ class DyttSpider(scrapy.Spider):
             for next_url in next_urls:
                 yield response.follow(next_url, callback=self.parse)
 
-        yield item
