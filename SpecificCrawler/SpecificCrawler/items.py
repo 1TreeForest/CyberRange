660b88715f903ff15ecab8d83c8409cf16167653
y0urOy <55057990+y0urOy@users.noreply.github.com>
2022-02-25 12:03:02 +0800

    Add files via upload

---
+++ SpecificCrawler/SpecificCrawler/items.py
@@ -0,0 +1,17 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ResultItem(scrapy.Item):
+    # define the fields for your item here like:
+    #电影名
+    name = scrapy.Field()
+    #详情页
+    link = scrapy.Field()
+    #爬取的电影网站
+    site = scrapy.Field()
+    pass
