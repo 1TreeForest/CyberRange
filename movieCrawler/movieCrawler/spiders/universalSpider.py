5e3cc0f4676bdc4a298563bd8f2dffba8003f99d
1TreeForest <1204572840@qq.com>
2022-02-27 17:28:39 +0800

    改进普适性爬虫

--- movieCrawler/movieCrawler/spiders/universalSpider.py
+++ movieCrawler/movieCrawler/spiders/universalSpider.py
@@ -16,8 +16,9 @@ class UniversalSpider(scrapy.Spider):
     item_list = []
     name = 'universalSpider'
     #  黑名单短语列表，若出现在name中则剔除
-    black_word_list = ['最新', 'topics', '会员', 'vip', 'VIP', '围观了', '点击图标', '分享到', '客户端', '热线', 'rss', 'RSS',
-                       '排行榜', '留言', '大全', '？？', '??', '频道', '影院', '站点地图', '跳转', '类型的电影']
+    black_word_list = ['topics', '会员', 'vip', 'VIP', '围观了', '点击图标', '分享到', '客户端', '热线', 'rss', 'RSS',
+                       '排行榜', '留言', '大全', '？？', '??', '频道', '站点地图', '跳转', '类型的电影', '话题', 'QQ', 'qq',
+                       ]
     #  黑名单题目列表，若与提取所得title相等则剔除
     black_title_list = ['招聘英才', '联系我们', '关于我们', '', '首页', '观看历史', '播放记录', '资讯', '分享', '评论', '生活',
                         '电影', '少儿', '剧情', '动作', '歌舞', '冒险', '惊悚', '悬疑', '剧情', '喜剧', '科幻', '爱情']
@@ -59,37 +60,31 @@ class UniversalSpider(scrapy.Spider):
         site_url = response.request.url
         item = UniversalItem()
         for tag in tag_list:
-            try:
-                title = tag.xpath('./@title').extract()[0]
-                href = tag.xpath('./@href').extract()[0]
-                if not href.startswith('http'):  # 处理本站内数据，即站内数据要加上前缀url
-                    if href[0] == '/' and site_url[-1] == '/':
-                        href = site_url + href[1:]
-                    elif href[0] == '/' and site_url[-1] != '/' or href[0] != '/' and site_url[-1] == '/':
-                        href = site_url + href
-                    elif href[0] != '/' and site_url[-1] != '/':
-                        href = site_url + '/' + href
-                    else:
-                        print('error!\n{}'.format(title, '\t', href, '\t', '*' * 100))
-                # title_in_brackets = re.search(r'《(.+?)》', title).group(1)  # 去除书名号，慎用
-                # if title_in_brackets:
-                #     title = title_in_brackets
-                item['link'] = href
-                item['name'] = title
-                item['site'] = self.item_list.get(site_url)
-                # print(item)
+            title = tag.xpath('./@title').extract()[0]
+            href = tag.xpath('./@href').extract()[0]
+            if not href.startswith('http'):  # 处理本站内数据，即站内数据要加上前缀url
+                if href[0] == '/' and site_url[-1] == '/':
+                    href = site_url + href[1:]
+                elif href[0] == '/' and site_url[-1] != '/' or href[0] != '/' and site_url[-1] == '/':
+                    href = site_url + href
+                elif href[0] != '/' and site_url[-1] != '/':
+                    href = site_url + '/' + href
+            # title_in_brackets = re.search(r'《(.+?)》', title).group(1)  # 去除书名号，慎用
+            # if title_in_brackets:
+            #     title = title_in_brackets
+            item['link'] = href
+            item['name'] = title
+            item['site'] = self.item_list.get(site_url)
+            # print(item)
 
-                if any(word in item['name'] for word in self.black_word_list) or \
-                        any(title == item['name'] for title in self.black_title_list) or \
-                        any(url == item['link'] for url in self.black_link_list or
-                        item['name'].isalnum()):  # 若不符合三个黑名单所定义的规则就剔除
-                    sql = 'insert ignore into `black_log` value("%s")' % item['name']
-                    self.cursor.execute(sql)
-                    self.conn.commit()
-                    continue
-
-                yield item
-                # print(item)
-
-            except Exception as e:
+            if any(word in item['name'] for word in self.black_word_list) or \
+                    any(title == item['name'] for title in self.black_title_list) or \
+                    any(url == item['link'] for url in self.black_link_list or
+                    item['name'].isalnum()):  # 若不符合三个黑名单所定义的规则就剔除
+                # sql = 'insert ignore into `black_log` value("%s")' % item['name']
+                # self.cursor.execute(sql)
+                # self.conn.commit()
                 continue
+
+            yield item
+            # print(item)
