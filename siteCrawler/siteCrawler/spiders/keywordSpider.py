9c8a21055bcc2ad33fe5298294245f54bc1f3410
1TreeForest <1204572840@qq.com>
2022-03-01 15:50:37 +0800

    一些部署

--- siteCrawler/siteCrawler/spiders/keywordSpider.py
+++ siteCrawler/siteCrawler/spiders/keywordSpider.py
@@ -28,7 +28,7 @@ class KeywordSpider(Spider):
     cursor = None
     urls_before = []
 
-    def __init__(self, keyword='all', se='bing', pages=50, *args, **kwargs):
+    def __init__(self, keyword='all', se='bing', pages=500, *args, **kwargs):
         super(KeywordSpider, self).__init__(*args, **kwargs)
         if keyword == 'all':  # 用户选择根据数据库中的词库进行搜索
             self.conn = pymysql.Connect(
