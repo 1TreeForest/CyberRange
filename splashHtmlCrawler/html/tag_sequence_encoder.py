b50df57c1f5687269bc3649d4b4409f45eab2e64
y0urOy <1559552844@qq.com>
2022-03-04 10:18:50 +0800

    添加多种相似度算法

--- splashHtmlCrawler/html/tag_sequence_encoder.py
+++ splashHtmlCrawler/html/tag_sequence_encoder.py
@@ -1,9 +1,8 @@
 import difflib
-
 import pymysql
 import os
 import re
-
+import Levenshtein
 
 class Encoder():
     pattern = None
@@ -22,8 +21,9 @@ class Encoder():
             charset='UTF8'
         )
         self.cursor = self.conn.cursor()
-        self.tag_white_dict = {'a': 'a', 'div': 'b', 'link': 'c', 'img': 'd', 'script': 'e', 'ul': 'f', 'li': 'g',
-                               'input': 'h', 'form': 'i', 'pre': 'j', 'noscript': 'k'}
+        self.tag_white_dict = {'a': 'a', 'link': 'c', 'img': 'd', 'script': 'e', 'ul': 'f', 'li': 'g',
+                               'input': 'h',
+                               'form': 'i'}
 
     def path_generator(self):
         for main_dir, sub_dir_list, file_name_list in os.walk('./pms'):
@@ -37,10 +37,13 @@ class Encoder():
             code = self.tag_white_dict.get(tag.group(1))
             if code is not None:
                 sequence += code
-        return sequence[:2000]
+
+        # print(sequence[:100])
+        return sequence[:500]
+        # return sequence
 
     def save_tag_sequence(self, domain, sequence):
-        sql = 'insert into tag_domain(tag_sequence, domain) values("%s","%s") on duplicate key update tag_sequence="%s"' % (sequence, domain, sequence)
+        sql = 'insert into tag_domain("tag_sequence", "domain") values("%s","%s")' % (sequence, domain)
         self.cursor.execute(sql)
         self.conn.commit()
 
@@ -48,6 +51,7 @@ class Encoder():
 if __name__ == '__main__':
     encoder = Encoder()
     file_generator = encoder.path_generator()
+    li = []
     while True:
         try:
             file = next(file_generator)
@@ -55,9 +59,24 @@ if __name__ == '__main__':
             file_name = file[1]
             with open(file_path, 'r', encoding='utf-8') as f:
                 # print(file_name)
-                sequence = encoder.get_tag_sequence(f)
-                encoder.save_tag_sequence(file_name[:-5], sequence)
-                print(len(sequence))
+                li.append((encoder.get_tag_sequence(f), file_name))
+                # encoder.save_tag_sequence(file_name, sequence)
         except StopIteration:
             print('All item processed')
             break
+    with open('./test.txt', 'w+')as f:
+        count_a = 1
+        for a in li[:200]:
+            c = a[0]
+            count_b = 1
+            for b in li:
+                d = b[0]
+                f.write(
+                    '\t'.join(['difflib similarity : ' + str(difflib.SequenceMatcher(None, c, d).ratio()), '\n' +
+                               'Levenshtein.ratio similarity : ' + str(Levenshtein.ratio(c, d)), '\n' +
+                               'Levenshtein.jaro similarity : ' + str(Levenshtein.jaro(c, d)), '\n' +
+                               'Levenshtein.jaro_winkler similarity : ' + str(Levenshtein.jaro_winkler(c, d)), '\n' +
+                               str(count_a), str(count_b), a[1], b[1],
+                               c[:50], d[:50]]) + '\n\n')
+                count_b += 1
+            count_a += 1
