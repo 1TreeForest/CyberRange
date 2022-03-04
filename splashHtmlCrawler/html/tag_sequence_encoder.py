4f0e0e0470f14ce8a469bf0f3139dd3906f57f7a
1TreeForest <1204572840@qq.com>
2022-03-04 15:39:01 +0800

    修改yyh合并的错误

--- splashHtmlCrawler/html/tag_sequence_encoder.py
+++ splashHtmlCrawler/html/tag_sequence_encoder.py
@@ -1,15 +1,11 @@
-import difflib
 import pymysql
 import os
 import re
-import Levenshtein
-
 class Encoder():
     pattern = None
     conn = None
     cursor = None
     tag_white_dict = None
-
     def __init__(self):
         self.pattern = re.compile(r'</?(\w+).')
         self.conn = pymysql.Connect(  # 配置数据库
@@ -21,15 +17,13 @@ class Encoder():
             charset='UTF8'
         )
         self.cursor = self.conn.cursor()
-        self.tag_white_dict = {'a': 'a', 'link': 'c', 'img': 'd', 'script': 'e', 'ul': 'f', 'li': 'g',
-                               'input': 'h',
-                               'form': 'i'}
+        self.tag_white_dict = {'a': 'a', 'div': 'b', 'link': 'c', 'img': 'd', 'script': 'e', 'ul': 'f', 'li': 'g',
+                               'input': 'h', 'form': 'i', 'pre': 'j', 'noscript': 'k'}
 
     def path_generator(self):
         for main_dir, sub_dir_list, file_name_list in os.walk('./pms'):
             for file_name in file_name_list:
                 yield [os.path.join(main_dir, file_name), file_name]  # 返回path与文件名组成的列表
-
     def get_tag_sequence(self, html):
         context = html.read()
         sequence = ''
@@ -37,13 +31,10 @@ class Encoder():
             code = self.tag_white_dict.get(tag.group(1))
             if code is not None:
                 sequence += code
-
-        # print(sequence[:100])
-        return sequence[:500]
-        # return sequence
+        return sequence[:2000]
 
     def save_tag_sequence(self, domain, sequence):
-        sql = 'insert into tag_domain("tag_sequence", "domain") values("%s","%s")' % (sequence, domain)
+        sql = 'insert into tag_domain(tag_sequence, domain) values("%s","%s") on duplicate key update tag_sequence="%s"' % (sequence, domain, sequence)
         self.cursor.execute(sql)
         self.conn.commit()
 
@@ -51,7 +42,6 @@ class Encoder():
 if __name__ == '__main__':
     encoder = Encoder()
     file_generator = encoder.path_generator()
-    li = []
     while True:
         try:
             file = next(file_generator)
@@ -59,28 +49,9 @@ if __name__ == '__main__':
             file_name = file[1]
             with open(file_path, 'r', encoding='utf-8') as f:
                 # print(file_name)
-                li.append((encoder.get_tag_sequence(f), file_name))
-                # encoder.save_tag_sequence(file_name, sequence)
+                sequence = encoder.get_tag_sequence(f)
+                encoder.save_tag_sequence(file_name[:-5], sequence)
+                print(len(sequence))
         except StopIteration:
             print('All item processed')
             break
-    with open('./test.txt', 'w+')as f:
-        count_a = 1
-        for a in li[:200]:
-            c = a[0]
-            count_b = 1
-            for b in li:
-                d = b[0]
-                f.write(
-                    '\t'.join([# 匹配的字符串数/sum, 其中sum是指str1和str2字串的长度总和
-                               'difflib similarity : ' + str(difflib.SequenceMatcher(None, c, d).ratio()), '\n' +
-                               # (sum-ldist)/sum, 其中sum是指str1和str2字串的长度总和，ldist是类编辑距离
-                               'Levenshtein.ratio similarity : ' + str(Levenshtein.ratio(c, d)), '\n' +
-                               # 计算jaro距离
-                               'Levenshtein.jaro similarity : ' + str(Levenshtein.jaro(c, d)), '\n' +
-                               # 计算jaro–Winkler距离
-                               'Levenshtein.jaro_winkler similarity : ' + str(Levenshtein.jaro_winkler(c, d)), '\n' +
-                               str(count_a), str(count_b), a[1], b[1],
-                               c[:50], d[:50]]) + '\n\n')
-                count_b += 1
-            count_a += 1
