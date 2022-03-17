d39822eca7333b78cf291d89e653475bd1400fe3
1TreeForest <1204572840@qq.com>
2022-03-17 19:25:10 +0800

    分组优化算法，增加计时和计数功能

--- splashHtmlCrawler/html/calculate_and_group.py
+++ splashHtmlCrawler/html/calculate_and_group.py
@@ -5,7 +5,7 @@ import Levenshtein
 import pymysql
 
 
-def slave(task_queue, group, grouped_item_dict, domain_list, item_dict, domain_list_len):
+def slave(task_queue, group, count, grouped_item_dict, domain_list, item_dict, domain_list_len):
     conn = pymysql.Connect(  # 配置数据库
         host='1.15.220.155',
         port=3306,
@@ -21,8 +21,10 @@ def slave(task_queue, group, grouped_item_dict, domain_list, item_dict, domain_l
             # (sum-ldist)/sum, 其中sum是指str1和str2字串的长度总和，ldist是类编辑距离
             similarity = Levenshtein.ratio(item_dict[domain_1][:task[1]],
                                            item_dict[domain_2][:task[1]])  # task[1]是用户选择的tag长度
-            print('\t'.join(['{0}:'.format(current_process().name), str(task[1]), domain_1, domain_2, str(similarity)]))
-            if similarity > 0.9:
+            # print('\t'.join(['{0}:'.format(current_process().name), str(task[1]), domain_1, domain_2, str(similarity)]))
+            count.value += 1
+            print('{0}:  {1}'.format(current_process().name, count.value))
+            if similarity >= 0.9:
                 exist_group = grouped_item_dict.get(domain_1)
                 if exist_group is not None:
                     sql = 'insert ignore into structure_group_test(group_tag, domain) values(%s, %s)'
@@ -38,9 +40,9 @@ def slave(task_queue, group, grouped_item_dict, domain_list, item_dict, domain_l
                     sql = 'insert ignore into structure_group_test(group_tag, domain) values(%s, %s)'
                     cursor.execute(sql, [group_num, domain_2])
                 conn.commit()
-            # sql = 'insert ignore into structure_similarity_{0}(domain_1, domain_2, similarity)values(%s,%s,%s) on duplicate key update similarity=%s'.format(
+            # sql = 'insert ignore into structure_similarity_{0}(domain_1, domain_2, similarity)values(%s,%s,%s)'.format(
             #     task[1])
-            # cursor.execute(sql, [domain_1, domain_2, similarity, similarity])
+            # cursor.execute(sql, [domain_1, domain_2, similarity])
             # conn.commit()
 
 
@@ -48,6 +50,7 @@ def master():
     NUMBER_OF_PROCESSES = 30  # 最大进程数量
     mgr = Manager()
     task_queue = Queue()
+    count = mgr.Value(int, 0)
     grouped_item_dict = mgr.dict()
     group = mgr.Value(int, 0)
     conn = pymysql.Connect(  # 配置数据库
@@ -66,7 +69,7 @@ def master():
     domain_list = list(item_dict.keys())
     domain_list_len = len(domain_list)
 
-    for tag_len in [250]:  # 对多长的tag进行对比
+    for tag_len in [600]:  # 对多长的tag进行对比
         for i in range(domain_list_len):
             task_queue.put([i, tag_len])
 
@@ -75,7 +78,7 @@ def master():
     time_start = time.time()
     for i in range(NUMBER_OF_PROCESSES):
         p = Process(target=slave,
-                    args=(task_queue, group, grouped_item_dict, domain_list, item_dict,
+                    args=(task_queue, group, count, grouped_item_dict, domain_list, item_dict,
                           domain_list_len))  # 创建进程，使用队列通信，共同完成队列中的任务
         p.start()
         proc_list.append(p)
